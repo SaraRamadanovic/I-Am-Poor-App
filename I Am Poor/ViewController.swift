1b90380349f864db0f267cf9eea0652b0bce941a
SaraRamadanovic <sara.ramadanovic@gmail.com>
2023-07-10 13:41:58 +0200

    Initial commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Sara Ramadanovic on 4. 10. 2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
